db6620a4907abb2568563e888e2dca9330e0b4e8
Julien <git.commit@takia.dev>
2024-09-11 11:08:53 +0400

    Update theme to Dark

--- AppData/Local/wezterm/wezterm.lua
+++ AppData/Local/wezterm/wezterm.lua
@@ -25,6 +25,21 @@ local config = {
 	  	font_size = 12.0,
 	  	-- active_titlebar_bg = "#7aa2f7",
 	  	-- inactive_titlebar_bg = "#292e42",
+	},
+	keys = {
+		{
+			key = 'c',
+			mods = 'CTRL',
+			action = wezterm.action_callback(function(window, pane)
+				selection_text = window:get_selection_text_for_pane(pane)
+				is_selection_active = string.len(selection_text) ~= 0
+				if is_selection_active then
+					window:perform_action(wezterm.action.CopyTo('ClipboardAndPrimarySelection'), pane)
+				else
+					window:perform_action(wezterm.action.SendKey{ key='c', mods='CTRL' }, pane)
+				end
+			end),
+		}
 	}
 }
 
