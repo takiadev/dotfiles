b502c7ba9d4184580e0e03a70223785e692d2d8e
Julien <git.commit@takia.dev>
2024-09-13 17:20:36 +0400

    Sav not working keys.lua

--- AppData/Local/nvim/lua/plugins/keys.lua
+++ AppData/Local/nvim/lua/plugins/keys.lua
@@ -39,36 +39,188 @@ return {
     },
 },
 {
-    "nvim-neo-tree/neo-tree.nvim",
-    keys = {
-        -- { "<leader>fe", function() require("neo-tree.command").execute({ toggle = true, dir = LazyVim.root() }) end, desc = "Explorer NeoTree (Root Dir)", },
-        -- { "<leader>fE", function() require("neo-tree.command").execute({ toggle = true, dir = vim.uv.cwd() }) end, desc = "Explorer NeoTree (cwd)", },
-        -- { "<leader>e", "<leader>fe", desc = "Explorer NeoTree (Root Dir)", remap = true },
-        -- { "<leader>E", "<leader>fE", desc = "Explorer NeoTree (cwd)", remap = true },
-        -- { "<leader>ge", function() require("neo-tree.command").execute({ source = "git_status", toggle = true }) end, desc = "Git Explorer", },
-        -- { "<leader>be", function() require("neo-tree.command").execute({ source = "buffers", toggle = true }) end, desc = "Buffer Explorer", },
-    },
-    opts = { window = { mappings = {
-        ["<2-LeftMouse>"] = "open",
-        ["i"] = "open",
-        ["h"] = "close_node",
-        ["<space>"] = "none",
-        ["Y"] = {
-            function(state)
-            local node = state.tree:get_node()
-            local path = node:get_id()
-            vim.fn.setreg("+", path, "c")
-            end,
-            desc = "Copy Path to Clipboard",
+  "nvim-neo-tree/neo-tree.nvim",
+	keys = {
+    -- { "<leader>fe", function() require("neo-tree.command").execute({ toggle = true, dir = LazyVim.root() }) end, desc = "Explorer NeoTree (Root Dir)", },
+    -- { "<leader>fE", function() require("neo-tree.command").execute({ toggle = true, dir = vim.uv.cwd() }) end, desc = "Explorer NeoTree (cwd)", },
+    -- { "<leader>e", "<leader>fe", desc = "Explorer NeoTree (Root Dir)", remap = true },
+    -- { "<leader>E", "<leader>fE", desc = "Explorer NeoTree (cwd)", remap = true },
+    -- { "<leader>ge", function() require("neo-tree.command").execute({ source = "git_status", toggle = true }) end, desc = "Git Explorer", },
+    -- { "<leader>be", function() require("neo-tree.command").execute({ source = "buffers", toggle = true }) end, desc = "Buffer Explorer", },
+  },
+  ---@param opts cmp.ConfigSchema
+  opts = function(_, opts)
+    opts.window = opts.window or {}
+    opts.window.mappings = { -- see https://github.com/MunifTanjim/nui.nvim/tree/main/lua/nui/popup for
+      ["r"] = "noop", -- left
+      ["s"] = "noop", -- down
+      ["f"] = "noop", -- up
+      ["t"] = "focus_preview", -- right
+
+      -- ["<LeftRelease>"] = "open",
+      ["i"] = "open",
+      ["h"] = "close_node",
+      ["<space>"] = "none",
+      ["Y"] = {
+          function(state)
+          local node = state.tree:get_node()
+          local path = node:get_id()
+          vim.fn.setreg("+", path, "c")
+          end,
+          desc = "Copy Path to Clipboard",
+      },
+      ["O"] = {
+          function(state)
+          require("lazy.util").open(state.tree:get_node().path, { system = true })
+          end,
+          desc = "Open with System Application",
+      },
+      ["P"] = { "toggle_preview", config = { use_float = false } },
+      ["<space>"] = {
+        "toggle_node",
+        nowait = false, -- disable `nowait` if you have existing combos starting with this char that you want to use
+      },
+      ["<2-LeftMouse>"] = "open",
+      ["<cr>"] = "open",
+      -- ["<cr>"] = { "open", config = { expand_nested_files = true } }, -- expand nested file takes precedence
+      ["<esc>"] = "cancel", -- close preview or floating neo-tree window
+      ["P"] = { "toggle_preview", config = { use_float = true, use_image_nvim = false } },
+      ["<C-f>"] = { "scroll_preview", config = { direction = -10 } },
+      ["<C-b>"] = { "scroll_preview", config = { direction = 10 } },
+      -- ["l"] = "focus_preview",
+      -- ["S"] = "open_split",
+      -- ["S"] = "split_with_window_picker",
+      -- ["s"] = "open_vsplit",
+      -- ["sr"] = "open_rightbelow_vs",
+      -- ["sl"] = "open_leftabove_vs",
+      -- ["s"] = "vsplit_with_window_picker",
+      ["t"] = "open_tabnew",
+      -- ["<cr>"] = "open_drop",
+      -- ["t"] = "open_tab_drop",
+      ["w"] = "open_with_window_picker",
+      ["C"] = "close_node",
+      ["z"] = "close_all_nodes",
+      --["Z"] = "expand_all_nodes",
+      ["R"] = "refresh",
+      ["a"] = {
+        "add",
+        -- some commands may take optional config options, see `:h neo-tree-mappings` for details
+        config = {
+          show_path = "none", -- "none", "relative", "absolute"
         },
-        ["O"] = {
-            function(state)
-            require("lazy.util").open(state.tree:get_node().path, { system = true })
-            end,
-            desc = "Open with System Application",
-        },
-        ["P"] = { "toggle_preview", config = { use_float = false } },
-    }}},
+      },
+      ["A"] = "add_directory", -- also accepts the config.show_path and config.insert_as options.
+      ["d"] = "delete",
+      ["r"] = "rename",
+      ["y"] = "copy_to_clipboard",
+      ["x"] = "cut_to_clipboard",
+      ["p"] = "paste_from_clipboard",
+      ["c"] = "copy", -- takes text input for destination, also accepts the config.show_path and config.insert_as options
+      ["m"] = "move", -- takes text input for destination, also accepts the config.show_path and config.insert_as options
+      ["e"] = "toggle_auto_expand_width",
+      ["q"] = "close_window",
+      ["?"] = "show_help",
+      ["<"] = "prev_source",
+      [">"] = "next_source",
+    }
+    opts.filesystem = opts.filesystem or {}
+    opts.filesystem.window = opts.filesystem.window or {} 
+    opts.filesystem.window.mappings = {
+      ["r"] = "noop", -- left
+      ["s"] = "noop", -- down
+      ["f"] = "noop", -- up
+      ["t"] = "focus_preview", -- right
+
+      ["H"] = "toggle_hidden",
+      ["/"] = "fuzzy_finder",
+      ["D"] = "fuzzy_finder_directory",
+      --["/"] = "filter_as_you_type", -- this was the default until v1.28
+      ["#"] = "fuzzy_sorter", -- fuzzy sorting using the fzy algorithm
+      -- ["D"] = "fuzzy_sorter_directory",
+      --["f"] = "filter_on_submit",
+      ["<C-x>"] = "clear_filter",
+      ["<bs>"] = "navigate_up",
+      ["."] = "set_root",
+      ["[g"] = "prev_git_modified",
+      ["]g"] = "next_git_modified",
+      ["i"] = "show_file_details",
+      ["o"] = { "show_help", nowait = false, config = { title = "Order by", prefix_key = "o" } },
+      ["oc"] = { "order_by_created", nowait = false },
+      ["od"] = { "order_by_diagnostics", nowait = false },
+      ["og"] = { "order_by_git_status", nowait = false },
+      ["om"] = { "order_by_modified", nowait = false },
+      ["on"] = { "order_by_name", nowait = false },
+      ["os"] = { "order_by_size", nowait = false },
+      ["ot"] = { "order_by_type", nowait = false },
+    }
+    opts.filesystem.window.fuzzy_finder_mappings = { -- define keymaps for filter popup window in fuzzy_finder_mode
+      ["<down>"] = "move_cursor_down",
+      ["<C-n>"] = "move_cursor_down",
+      ["<up>"] = "move_cursor_up",
+      ["<C-p>"] = "move_cursor_up",
+    }
+    opts.buffer = opts.buffer or {}
+    opts.buffer.window = opts.buffer.window or {}
+    opts.buffer.window.mappings = {
+      ["<bs>"] = "navigate_up",
+      ["."] = "set_root",
+      ["bd"] = "buffer_delete",
+      ["i"] = "show_file_details",
+      ["o"] = { "show_help", nowait = false, config = { title = "Order by", prefix_key = "o" } },
+      ["oc"] = { "order_by_created", nowait = false },
+      ["od"] = { "order_by_diagnostics", nowait = false },
+      ["om"] = { "order_by_modified", nowait = false },
+      ["on"] = { "order_by_name", nowait = false },
+      ["os"] = { "order_by_size", nowait = false },
+      ["ot"] = { "order_by_type", nowait = false },
+    }
+    opts.git_status = opts.git_status or {}
+    opts.git_status.window = opts.git_status.window or {}
+    opts.git_status.window.mappings = {
+      ["A"] = "git_add_all",
+      ["gu"] = "git_unstage_file",
+      ["ga"] = "git_add_file",
+      ["gr"] = "git_revert_file",
+      ["gc"] = "git_commit",
+      ["gp"] = "git_push",
+      ["gg"] = "git_commit_and_push",
+      ["i"] = "show_file_details",
+      ["o"] = { "show_help", nowait = false, config = { title = "Order by", prefix_key = "o" } },
+      ["oc"] = { "order_by_created", nowait = false },
+      ["od"] = { "order_by_diagnostics", nowait = false },
+      ["om"] = { "order_by_modified", nowait = false },
+      ["on"] = { "order_by_name", nowait = false },
+      ["os"] = { "order_by_size", nowait = false },
+      ["ot"] = { "order_by_type", nowait = false },
+    }
+    opts.document_symbols = opts.document_symbols or {}
+    opts.document_symbols.window = opts.documents_synbols.window or {}
+    opts.document_symbols.window.mappings = {
+      ["r"] = "noop", -- left
+      ["s"] = "noop", -- down
+      -- ["f"] = "noop", -- up
+      ["t"] = "noop", -- right
+
+      ["<cr>"] = "jump_to_symbol",
+      ["o"] = "jump_to_symbol",
+      ["A"] = "noop", -- also accepts the config.show_path and config.insert_as options.
+      ["d"] = "noop",
+      ["y"] = "noop",
+      ["x"] = "noop",
+      ["p"] = "noop",
+      ["c"] = "noop",
+      ["m"] = "noop",
+      ["a"] = "noop",
+      ["/"] = "filter",
+      -- ["f"] = "filter_on_submit",
+    }
+    if opts.example and opts.example.window then
+      opts.example.window.mappings = {}
+    end
+    -- { ["<cr>"] = "toggle_node", ["<C-e>"] = "example_command", ["d"] = "show_debug_info", }
+
+    return opts
+  end,
 },
 {
     "akinsho/bufferline.nvim",
